18c36a774ee2efcc37dd7243a058245ea7fb2571
parth1234560 <pathakparthixc7503@gmail.com>
2026-09-11 21:44:07 +0530

    Fix CodePipeline V2 push trigger

--- terraform/modules/cicd/main.tf
+++ terraform/modules/cicd/main.tf
@@ -127,7 +127,7 @@ resource "aws_codepipeline" "app" {
         ConnectionArn    = var.github_connection_arn
         FullRepositoryId = var.github_repository
         BranchName       = var.github_branch
-        DetectChanges    = "true"
+        DetectChanges    = "false"
       }
     }
   }
